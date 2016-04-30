5f5a588bfa69decadbd92b1ea067d37f6c5f353a
Shigenobu Nishikawa <shishi.s.n@gmail.com>
2016-04-30 13:17:13 +0900

    add rspec configure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,9 @@
 require File.expand_path('../../lib/rash', __FILE__)
 
 require 'rspec'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+end
